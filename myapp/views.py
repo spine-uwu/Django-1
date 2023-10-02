513b821adacb09ced63ddcd0ce2c16820e451d51
Spine <118034874+spine-uwu@users.noreply.github.com>
2023-10-03 02:19:15 +0600

    Add files via upload

---
+++ myapp/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render,HttpResponse
+from .models import TodoItem
+# Create your views here.
+
+def home(request):
+    return render(request,'home.html')
+
+def todo(request):
+    items = TodoItem.objects.all()
+    return render(request,'todo.html',{"todos": items})
+    
